4ff4b02227858c80bd781b163d560999b69bfd41
ldobrenkonikital <ldobrenkonikital@gmail.com>
2018-12-11 16:06:47 +0200

    UI&BasicDocumentation
    chore:
    1) add basic documentation
    2) add background collor
    3) add css style to "uploader"
    4) add js logic to show the name of uploding file or quantity of files
    5) add css for "get duplicates" button
    6) add logo
    7) add favicon
    8) add css for logo (round)
    9) create "info" window
    10) add css to "info" window
    11) create "info" button
    12) add css to "info" button
    13) add css to normalize sizes & fonts on the page
    14) add js logic and made "info window" floating window
    refactor:
    1) move app.js to "src" directory
    fix:
    1) move a string where input file is being rad into the module exports in duplicatesController.js, because i need it it  to be rad only after click the button

---
+++ src/app.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const router = require('./server/router');
+const bodyParser = require('body-parser');
+
+const app = express();
+router(app);
+
+app.use(bodyParser.json());
+app.use(express.static('src/public'));
+
+const server = app.listen(2000, () => {
+    const port = server.address().port;
+    console.log('Listening at http://' + 'localhost' +  ':' + port);
+});
